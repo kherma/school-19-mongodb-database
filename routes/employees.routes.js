5ffc57933ae4a97f505bebe3b0ef04cab3a2ef68
kherma <kamil.herma01@gmail.com>
2021-04-03 18:23:23 +0200

    Connect products endpoints to mongoDB

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const ObjectId = require("mongodb").ObjectId;
-const db = require("./../db");
 
 router.get("/employees", (req, res) => {
   req.db
